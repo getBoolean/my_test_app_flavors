bcbafec49a056ae41d10af20079d9b8ec3d6b894
getBoolean <danielwnewville@gmail.com>
2022-02-25 00:25:49 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/getboolean/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.getboolean.my_test_app_flavors
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2021, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:flutter/cupertino.dart';
+import 'package:my_test_app_flavors/app/app.dart';
+
+void main() {
+  runApp(const App());
+}
